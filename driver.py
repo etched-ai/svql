7abe4c1beaf205afdc1ab121155bff2ea4c584ed
Sathvik Redrouthu <sathvik@breeze2.sv8.i.etched.com>
2025-02-28 02:03:38 -0800

    Updated for python package.

--- driver.py
+++ driver.py
@@ -3,9 +3,11 @@ from core.module import Module
 # Example usage
 module = Module("tests/rtl/pipeline_top.sv")
 
-# Example usage with SQL queries
-print(f"All parameters that have not been changed from default for {module.name}:")
-print(module.query("SELECT * FROM params WHERE override_value IS NULL"), "\n")
 
-print(f"All disconnected ports for {module.name}:")
-print(module.query("SELECT * FROM ports WHERE connected_to IS NULL"), "\n")
+print(module.ports)
+# Example usage with SQL queries
+# print(f"All parameters that have not been changed from default for {module.name}:")
+# print(module.query("SELECT * FROM params WHERE override_value IS NULL"), "\n")
+
+# print(f"All disconnected ports for {module.name}:")
+# print(module.query("SELECT * FROM ports WHERE connected_to IS NULL"), "\n")
